0617a1ae45383aa1571cee5c39b72521f2c612ea
Jelte Fennema <jelte12345@gmail.com>
2012-02-21 20:42:35 +0100

    ff weer eens gecommit

--- 2/2.c
+++ 2/2.c
@@ -1,3 +1,17 @@
+/*
+ * File 2.c
+ * 
+ * This file contains the code for assignment 2. It decodes a text file that is
+ * encoded using Caesar replacement. It uses the first parameter as the encoded
+ * file and the second as the frequencies file.
+ * 
+ * Author: Jelte Fennema
+ * Studentnumber: 10183159
+ * Date: 17-02-2012
+ *
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -6,98 +20,116 @@
 
 
 FILE* open_file(char *filename);
-int msg_size(char *msg_ptr);
-int check_possibilities(char *msg_ptr, char letters[], double freqs[]);
+int msg_size(FILE *msg_ptr);
+int check_possibilities(FILE *msg_ptr, char letters[], double freqs[]);
+
+/*
+ * This is the main function. It opens and closes the files. It also prints the
+ * final output. 
+ */
 int main(int argc, char *argv[]){
-    if(argc < 3)
+    if (argc < 3){
         printf("Not enough parameters have been passed\n");
-    else{
-        int i, c, offset;
-        char letters[26];
-        double freqs[26];
-        FILE *msg_ptr, *freq_ptr;
-        freq_ptr = open_file(argv[2]);
-        for(i = 0; i<26; i++){
-            fscanf(freq_ptr, "%c %lf\n", &letters[i], &freqs[i]);
-            printf("%d: %c %lf\n", i, letters[i], freqs[i]);
+        return(0);
+    }
+    
+    int i, c, offset;
+    char letters[26];
+    double freqs[26];
+    FILE *msg_ptr, *freq_ptr;
+    msg_ptr = open_file(argv[1]);
+    freq_ptr = open_file(argv[2]);
+    for (i = 0; i<26; i++){
+        fscanf(freq_ptr, "%c %lf\n", &letters[i], &freqs[i]);
+    }
+    fclose(freq_ptr);
+    offset = check_possibilities(msg_ptr, letters, freqs);
+    printf("\nThe decoded text is:\n");
+    while ((c = getc(msg_ptr)) != EOF){ 
+        if (isupper(c)){
+            c = c-offset;
+            if (c < 'A')
+                c += 26;
         }
-        offset = check_possibilities(argv[1], letters, freqs);
-        msg_ptr = open_file(argv[1]);
-        while((c = getc(msg_ptr)) != EOF){ 
-            if(isupper(c)){
-                c = c-offset;
-                if(c < 'A')
-                    c += 26;
-            }
-            else if(islower(c)){
-                c = c-offset;
-                if(c < 'a')
-                    c += 26;
-            }
-            printf("%c", c);
+        else if (islower(c)){
+            c = c-offset;
+            if (c < 'a')
+                c += 26;
         }
-	}
+        printf("%c", c);
+    }
+    fclose(msg_ptr);
 }
 
+/*
+ * This function opens a file and returns the pointer to the file.
+ */
 FILE* open_file(char *filename){
     FILE *file;
-    if((file = fopen(filename, "r")) == NULL){
+    if ((file = fopen(filename, "r")) == NULL){
         perror("fopen");
         exit(0);
     }
     return file;
 }
-int msg_size(char *msg_ptr){
+
+/*
+ * This function returns the amount of alphabetic characters that are in a file.
+ */
+int msg_size(FILE *msg_ptr){
     int msg_size = 0;
     char c;
-    FILE *msg_file;
-    msg_file = open_file(msg_ptr);
-    while((c = getc(msg_file)) !=  EOF){
-        if(!isalpha(c)){
+    while ((c = getc(msg_ptr)) !=  EOF){
+        if (!isalpha(c)){
             continue;
         }
         msg_size++;
     }
+    rewind(msg_ptr);
     return msg_size;
 }
-int check_possibilities(char *msg_ptr, char letters[], double freqs[]){
-    int k, h, i = 0, final = 0, length=msg_size(msg_ptr);
+
+/*
+ * This function calculates the letter frequencies of the file using Caeser
+ * replacement. It returns the one with the smallest absolute difference.
+ */
+int check_possibilities(FILE *msg_file, char letters[], double freqs[]){
+    int k, h, i = 0, final = 0, length=msg_size(msg_file);
     double freq, margin, best_margin=2600;
     char c, letter, text[length];
-    FILE *msg_file;
-    msg_file = open_file(msg_ptr);
-    while((c = getc(msg_file)) != EOF){
-        if(!isalpha(c)){
+    while ((c = getc(msg_file)) != EOF){
+        if (!isalpha(c)){
             continue;
         }
         text[i] = tolower(c);
         i++;
     }
-    for(i = 0; i < 26; i++){
+    rewind(msg_file);
+    for (i = 0; i < 26; i++){
         int counts[26] = {0};
-        printf("%d: ", i);
-        for(k = 0; k < length; k++){
+        for (k = 0; k < length; k++){
             letter = text[k]-i;
-            if(letter < 'a')
+            if (letter < 'a')
                 letter += 26;
-            printf("%c", letter);
-            for(h = 0; h < 26; h++){
-                if(letter == letters[h]){
+            for (h = 0; h < 26; h++){
+                if (letter == letters[h]){
                     counts[h]++;
                 }
             }
         }
         margin = 0;
-        for(k = 0; k < 26; k++){
+        for (k = 0; k < 26; k++){
             freq = (double)counts[k]/length*100;
             margin += fabs(freqs[k] - freq);
         }
+        printf("%d", i);
         if (margin < best_margin){
             best_margin = margin;
             final = i;
             printf("*");
         }
-        printf("\n");
+        printf(" ");
     }
+    printf("\n");
     return final;
 }
