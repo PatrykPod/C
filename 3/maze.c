2fab0ebfa44f097d83ff7e69ebbc7ee58f3e4397
Jelte Fennema <jelte12345@gmail.com>
2012-02-21 21:03:14 +0100

    opdracht 3 gedownload

---
+++ 3/maze.c
@@ -0,0 +1,18 @@
+#include "maze.h"
+
+/* Incomplete definitions of the maze support function . */
+maze_t* init_maze( ... ) {
+    ...
+}
+
+maze_t* read_maze( ... ) {
+    ...
+}
+
+void print_maze( ... ) {
+    ...
+}
+
+void cleanup_maze ( ... ) {
+    ...
+}
